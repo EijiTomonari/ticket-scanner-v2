2e1bc39863e17e5ab8b7c29e636b0ea4018a525f
Eiji Tomonari <gabriel@tomonari.dev>
2022-08-04 21:01:36 -0300

    Add route for reading all settings from database

--- src/server.py
+++ src/server.py
@@ -1,6 +1,33 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+import json
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'config/config.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Setting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    value = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Setting {self.name}>'
+
+    def toJSON(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'value': self.value
+        }
 
 
 @app.route("/")
@@ -19,4 +46,15 @@ def write_settings():
 
 @app.route("/settings/read")
 def read_settings():
-    return "Read settings"
+    """
+    It takes all the settings from the database, converts them to JSON, and then puts them into a
+    dictionary.
+    :return: A JSON object with the settings.
+    """
+    settings = Setting.query.all()
+    settingsObject = {}
+    json_list = [i.toJSON() for i in settings]
+    for setting in json_list:
+        settingsObject[setting['name']] = setting
+    print(settingsObject)
+    return jsonify(settingsObject)
